ad696484ae1f3a7ebcc7fa93a2a1fd1190372282
Alex Koppel <alex+git@alexkoppel.com>
2012-08-08 17:05:01 +0200

    Batch controller spec

--- app/controllers/batch_api/batch_controller.rb
+++ app/controllers/batch_api/batch_controller.rb
@@ -1,6 +1,10 @@
+require 'batch_api/operation'
+
 module BatchApi
   class BatchController < ::ApplicationController
     def batch
+      ops = params[:ops].map {|o| BatchApi::Operation.new(o, request.env)}
+      render :json => ops.map(&:execute)
     end
   end
 end
---
+++ spec/controllers/batch_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+require 'batch_api/operation'
+
+describe BatchApi::BatchController do
+  describe "#batch" do
+    it "creates batch ops for each operation using the request environment" do
+      ops = 10.times.collect {|i| ({"operation" => i.to_s}) }
+      ops.each do |o|
+        BatchApi::Operation.should_receive(:new).with(o, request.env).and_return(stub(:execute => ""))
+      end
+
+      xhr :post, :batch, ops: ops
+    end
+
+    it "returns the result of the batch operation's execution in order" do
+      ops = 10.times.collect {|i| {"operation" => i.to_s} }
+      ops.each do |o|
+        BatchApi::Operation.should_receive(:new).and_return(stub(:execute => o["operation"]))
+      end
+
+      xhr :post, :batch, ops: ops
+      json = JSON.parse(response.body)
+      ops.each_with_index do |o, i|
+        json[i].should == i.to_s
+      end
+    end
+  end
+end
