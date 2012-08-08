850132386df69d1260c72e687a6c338af9b60ea9
Alex Koppel <alex+git@alexkoppel.com>
2012-08-08 12:49:50 +0200

    Remove fixed config in favor of route-based config

--- lib/batch_api.rb
+++ lib/batch_api.rb
@@ -1,14 +1,6 @@
-require 'batch_api/configuration'
+require 'batch_api/routing_helper'
 require 'batch_api/version'
 require 'active_support/dependencies'
 
 module BatchApi
-  mattr_accessor :config
-  def self.config
-    @@config || Configuration.new
-  end
-
-  def self.setup
-    yield (self.config = Configuration.new)
-  end
 end
--- spec/lib/batch_api_spec.rb
+++ spec/lib/batch_api_spec.rb
@@ -2,25 +2,4 @@ require 'spec_helper'
 require 'batch_api'
 
 describe BatchApi do
-  describe ".setup" do
-    it "has a reader for config" do
-      BatchApi.config.should_not be_nil
-    end
-
-    it "provides a default config" do
-      BatchApi.config.should be_a(BatchApi::Configuration)
-    end
-
-    it "yields a new config object" do
-      BatchApi.setup do |c|
-        c.should be_a(BatchApi::Configuration)
-      end
-    end
-
-    it "stores the config object" do
-      expected = nil
-      BatchApi.setup {|c| expected = c}
-      BatchApi.config.should == expected
-    end
-  end
 end
