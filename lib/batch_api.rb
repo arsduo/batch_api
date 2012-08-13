95dedb274e61aa2c37947f7139f5d98135984af8
Alex Koppel <alex+git@alexkoppel.com>
2012-08-13 18:52:22 +0200

    Add config accessor here

--- lib/batch_api.rb
+++ lib/batch_api.rb
@@ -2,6 +2,15 @@ require 'batch_api/routing_helper'
 require 'batch_api/engine'
 require 'batch_api/configuration'
 require 'batch_api/version'
+require 'batch_api/processor'
 
 module BatchApi
+  mattr_accessor :config
+  def self.config
+    @@config || Configuration.new
+  end
+
+  def self.setup
+    yield (self.config = Configuration.new)
+  end
 end
--- spec/lib/batch_api_spec.rb
+++ spec/lib/batch_api_spec.rb
@@ -2,4 +2,27 @@ require 'spec_helper'
 require 'batch_api'
 
 describe BatchApi do
+  describe ".config" do
+    it "has a reader for config" do
+      BatchApi.config.should_not be_nil
+    end
+
+    it "provides a default config" do
+      BatchApi.config.should be_a(BatchApi::Configuration)
+    end
+  end
+
+  describe ".setup" do
+    it "yields a new config object" do
+      BatchApi.setup do |c|
+        c.should be_a(BatchApi::Configuration)
+      end
+    end
+
+    it "stores the config object" do
+      expected = nil
+      BatchApi.setup {|c| expected = c}
+      BatchApi.config.should == expected
+    end
+  end
 end
