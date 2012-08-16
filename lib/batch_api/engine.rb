646c82a629b8a3ae36643e722e4d3124808b3ec7
Alex Koppel <alex+git@alexkoppel.com>
2012-08-16 17:53:28 +0200

    Remove unneeded files

--- lib/batch_api/engine.rb
+++
@@ -1,8 +0,0 @@
-# CURRENT FILE :: lib/team_page/engine.rb
-module BatchApi
-  class Engine < Rails::Engine
-    initializer "batch_api.add_routing_helper" do |app|
-      ActionDispatch::Routing::Mapper.send(:include, BatchApi::RoutingHelper)
-    end
-  end
-end
