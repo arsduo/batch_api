ba333ce6512d96255cd5b5bcc55d85f5a451c9bb
Alex Koppel <alex+git@alexkoppel.com>
2012-09-20 18:24:48 +0200

    Updated documentation

--- lib/batch_api/internal_middleware.rb
+++ lib/batch_api/internal_middleware.rb
@@ -8,21 +8,27 @@ module BatchApi
   # (Not to be confused with RackMiddleware, which handles incoming Rack
   # request.)  Based on Mitchel Hashimoto's middleware gem.
   #
-  # The middleware stack is defined in a block, and has three phases:
-  #   1) Global - these middlewares will be run around the entire sequence of
+  # The middleware stack is defined in a block, and has four phases:
+  #   1) Batch - these middlewares will be run around the entire sequence of
   #   batch requests.  Useful for global processing on a batch request; for
   #   instance, the timestamp middleware adds a timestamp based on when the
-  #   original request was# started.
+  #   original request was started.  This should return an array of
+  #   BatchApi::Response objects.
   #
   #   2) Processor - this automatically-provided middleware will execute all
   #   batch requests either sequentially (currently the only option) or in
-  #   parallel (in the future).
+  #   parallel (in the future).  This will return an array of
+  #   BatchApi::Response objects.
   #
-  #   3) Per-op - these middlewares will run once per each operation, giving
+  #   3) Operation - these middlewares will run once per each operation, giving
   #   you a chance to alter the results or details of an op -- for instance,
-  #   decoding the body if it's JSON.
+  #   decoding the body if it's JSON.  This should return an individual
+  #   BatchApi::Response object.
   #
-  # All middlewares will receive the following as an env hash:
+  #   4) Executor - this automatically-provided middleware actually executes
+  #   the individual Rack request, and returns a BatchApi::Response object.
+  #
+  # All middlewares#call will receive the following as an env hash:
   #   {
   #     ops: [], # the total set of operations
   #     op: obj, # the specific operation being executed, if appropriate
@@ -32,6 +38,7 @@ module BatchApi
   #
   # All middlewares should return the result of their individual operation or
   # the array of operation results, depending on where they are in the chain.
+  # (See above.)
   module InternalMiddleware
     # Public: the default internal middlewares to be run around the entire
     # operation.
