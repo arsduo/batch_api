723ebf6dbb403985a79077da4cb82fcbbbf81142
Alex Koppel <alex+git@alexkoppel.com>
2012-09-20 17:41:18 +0200

    Fix response spec

--- spec/lib/response_spec.rb
+++ spec/lib/response_spec.rb
@@ -17,37 +17,11 @@ describe BatchApi::Response do
     response.status.should == raw_response.first
   end
 
-  it "sets headers to the HTTP headers" do
-    response.headers.should == raw_response[1]
+  it "sets body to the HTTP body turned into a string" do
+    response.body.should == raw_response[2].join
   end
 
-  describe "the body" do
-    context "for non-JSON requests" do
-      before :each do
-        raw_response[1]["Content-Type"] = "text/html"
-      end
-
-      it "sets body to the string representation of the response body" do
-        response.body.should == raw_response[2].join
-      end
-    end
-
-    context "for JSON responses" do
-      let(:json) { {"a" => 2, "b" => {"c" => 3}} }
-
-      before :each do
-        raw_response[1]["Content-Type"] = "application/json"
-        raw_response[2] = [json.to_json]
-      end
-
-      it "decodes the body if the decode_json_responses is set" do
-        response.body.should == json
-      end
-
-      it "doesn't decode the body if decode_json_responses is false" do
-        BatchApi.config.stub(:decode_json_responses).and_return(false)
-        response.body.should == json.to_json
-      end
-    end
+  it "sets headers to the HTTP headers" do
+    response.headers.should == raw_response[1]
   end
 end
