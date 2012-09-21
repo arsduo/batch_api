c54ef4cae1f8f1470a69a33fd03a4b014750de4c
Alex Koppel <alex+git@alexkoppel.com>
2012-09-22 00:13:53 +0200

    Create ErrorWrapper class (it's not an error!)

--- spec/lib/error_wrapper_spec.rb
+++ spec/lib/error_wrapper_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
-require 'batch_api/errors/base'
+require 'batch_api/error_wrapper'
 
-describe BatchApi::Errors::Base do
+describe BatchApi::ErrorWrapper do
   let(:exception) {
     StandardError.new(Faker::Lorem.words(3)).tap do |e|
       e.set_backtrace(Kernel.caller)
     end
   }
 
-  let(:error) { BatchApi::Errors::Base.new(exception) }
+  let(:error) { BatchApi::ErrorWrapper.new(exception) }
 
   describe "#body" do
     it "includes the message in the body" do
@@ -53,9 +53,9 @@ describe BatchApi::Errors::Base do
   describe ".expose_backtrace?" do
     it "returns false if Rails.env.production?" do
       Rails.env.stub(:production?).and_return(true)
-      BatchApi::Errors::Base.expose_backtrace?.should be_false
+      BatchApi::ErrorWrapper.expose_backtrace?.should be_false
       Rails.env.stub(:production?).and_return(false)
-      BatchApi::Errors::Base.expose_backtrace?.should be_true
+      BatchApi::ErrorWrapper.expose_backtrace?.should be_true
     end
   end
 end
